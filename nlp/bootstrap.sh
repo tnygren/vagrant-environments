4998cfe675d33d9d95ece289065f382b01941282
Teppo Nygren <teppo.nygren@iki.fi>
2016-03-09 19:16:52 +0200

    Add enviroment for natural language processing

---
+++ nlp/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt-get update
+#apt-get install -y apache2 vim xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+#apt-get install -y apache2 vim
+apt-get install -y apache2 vim git zsh
+
+#python
+apt-get install -y python-sklearn python-nltk python-qt4
+
+#java
+apt-get install -y default-jdk
+ln -s /vagrant/ .
+
+#sudo VBoxClient-all
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+#NLTK
+python -m nltk.downloader brown
+python -m nltk.downloader punkt
