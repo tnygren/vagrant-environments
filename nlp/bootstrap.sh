4b09160f23df543df9c5e6e4d40552494c07adcc
Teppo Nygren <teppo.nygren@iki.fi>
2016-03-18 07:10:45 +0200

    Change box release version to Ubuntu 15.10 Wily Werewolf
    Remove apache and java
    Fix mount problem after guest os restart

--- nlp/bootstrap.sh
+++ nlp/bootstrap.sh
@@ -2,15 +2,14 @@
 
 apt-get update
 #apt-get install -y apache2 vim xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-#apt-get install -y apache2 vim
-apt-get install -y apache2 vim git zsh
+#apt-get install -y apache2 vim git zsh
+apt-get install -y vim git zsh
 
 #python
 apt-get install -y python-sklearn python-nltk python-qt4 python-pandas
 
 #java
-apt-get install -y default-jdk
-ln -s /vagrant/ .
+#apt-get install -y default-jdk
 
 #sudo VBoxClient-all
 if ! [ -L /var/www ]; then
@@ -23,3 +22,5 @@ python -m nltk.downloader brown
 python -m nltk.downloader punkt
 python -m nltk.downloader stopwords
 python -m nltk.downloader wordnet
+
+apt-get autoremove
